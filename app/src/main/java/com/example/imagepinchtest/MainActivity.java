638131905a472908f055408774d0ffd649080043
habilithm97 <habinkim97@gmail.com>
2022-11-02 23:52:39 +0900

    PhotoView 라이브러리를 이용한 이미지뷰 핀치줌

--- app/src/main/java/com/example/imagepinchtest/MainActivity.java
+++ app/src/main/java/com/example/imagepinchtest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.imagepinchtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PhotoViewAttacher attacher;
+        ImageView iv = (ImageView)findViewById(R.id.iv);
+        attacher = new PhotoViewAttacher(iv);
     }
 }
